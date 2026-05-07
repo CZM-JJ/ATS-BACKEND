43c73cc4525d2ca171de4393fbef56f309c6cbd1
skarpskytt <jchua@czarkmak.com>
2026-05-07 14:37:19 +0800

    Fix invalid Eloquent method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,26 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        // Use optimized queries in production
         if ($this->app->environment('production')) {
-            Model::preventSilentlyFailingAttributes();
+            Model::preventSilentlyDiscardingAttributes();
             Model::preventAccessingMissingAttributes();
         }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Optimize for production
         if ($this->app->environment('production')) {
-            // Use HTTPS URLs
             if (config('app.url')) {
                 \Illuminate\Support\Facades\URL::forceScheme('https');
                 \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
@@ -34,4 +25,3 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 }
-
